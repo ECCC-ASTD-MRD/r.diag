76509d5a5c057bc546363e217c142ef5e5b06e05
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2022-07-19 17:59:39 +0000

    Fixed bin build with cmake

--- src/lssub/convsub.f
+++ src/lssub/convsub.f
@@ -7,22 +7,6 @@
 #     if !defined (unite_io_maximale)
 #         define  unite_io_maximale 99
 #     endif
-#     if defined (RDIAG_LICENCE)
-!---------------------------------- LICENCE BEGIN -------------------------------
-! R.DIAG - Diagnostic tool kit for the GEM numerical atmospheric model
-! Copyright (C) 1990-2010 - Division de Recherche en Prevision Numerique
-!                       Environnement Canada
-! This code is free software; you can redistribute it and/or modify it 
-! under the terms of the GNU Lesser General Public License as published by
-! the Free Software Foundation, version 2.1 of the License. This library is
-! distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-! without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
-! PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-! You should have received a copy of the GNU Lesser General Public License
-! along with this library; if not, write to the Free Software Foundation, Inc.,
-! 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-!---------------------------------- LICENCE END ---------------------------------
-#     endif
 !     $Log: convsub.ftn,v $
 !     Revision 3.135  2021/01/26  dugas
 !     - Modifier le code qui identifie les intervals
--- src/lssub/paccrn.f
+++ src/lssub/paccrn.f
@@ -1,22 +1,6 @@
 #     if !defined (nombre_de_taches)
 #         define   nombre_de_taches 1
 #     endif
-#     if defined (RDIAG_LICENCE)
-!---------------------------------- LICENCE BEGIN -------------------------------
-! R.DIAG - Diagnostic tool kit for the GEM numerical atmospheric model
-! Copyright (C) 1990-2010 - Division de Recherche en Prevision Numerique
-!                       Environnement Canada
-! This code is free software; you can redistribute it and/or modify it 
-! under the terms of the GNU Lesser General Public License as published by
-! the Free Software Foundation, version 2.1 of the License. This library is
-! distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-! without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
-! PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-! You should have received a copy of the GNU Lesser General Public License
-! along with this library; if not, write to the Free Software Foundation, Inc.,
-! 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-!---------------------------------- LICENCE END ---------------------------------
-#     endif
 C     $Log: paccrn.ftn,v $
 C     Revision 3.13  2016/10/26 15:29  dugas
 C     Modification syntaxique F77 --> F90
